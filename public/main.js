24ab5293a70b4186fd569f7b7ff55d2512288fc2
MinionMax <maxime.mion@gmx.de>
2021-01-22 20:27:15 +0100

    socket implementation: play, pause

--- public/main.js
+++ public/main.js
@@ -1,12 +1,10 @@
-// 2. This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
+var socket = io();
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player('player', {
@@ -25,28 +23,58 @@ function onYouTubeIframeAPIReady() {
 	});
 }
 
-// 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
 	event.target.playVideo();
 }
 
-const interval = null
+
+var interval = null
 function onPlayerStateChange(event) {
 	if(event.data === YT.PlayerState.PLAYING){
 		interval = setInterval(progressLoop, 200)
 	}
 	else{
 		if(interval) clearInterval(interval)
-	};
+	}
 }
 
 function progressLoop(){
 	var bar = document.querySelector(".progress");
 	var cursor = document.querySelector(".cursor");
-	// bar.addEventListener("click",function(){
-	// 	var offset = this.offset();
-	// })
-
 	var fraction = player.getCurrentTime()/player.getDuration()*100;
 	cursor.style.left = fraction.toString() + "%";
 }
+
+function playVideo(){
+	player.playVideo();
+}
+
+function pauseVideo(){
+	player.pauseVideo();
+}
+
+function initBtns(){
+	var playb = document.querySelector(".play");
+	var pauseb = document.querySelector(".pause");
+	playb.addEventListener("click", () => {
+		currData = { playerStatus: "play", time: player.getCurrentTime() };
+		socket.emit("playerEvent", currData)
+	})
+	
+	pauseb.addEventListener("click", () => {
+		currData = { playerStatus: "pause", time: player.getCurrentTime() };
+		socket.emit("playerEvent", currData)
+	})
+}
+initBtns();
+
+
+socket.on("playerEvent", (data) => {
+	if(data.playerStatus === "play"){
+		playVideo();
+		console.log(data);
+	}else if(data.playerStatus === "pause"){
+		pauseVideo();
+		console.log(data);
+	}
+})
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "public", "index.html");
+});
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+    });
+    socket.on("playerEvent", (data) => {
+        io.emit("playerEvent", data)
+        console.log(data)
+    })
+});
+
+http.listen(PORT, () => console.log(`Listening on ${PORT}`));
